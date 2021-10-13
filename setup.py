e1cbf0c04f9bc73cd00c751df76089802fd12284
mengxiang <xmeng@elsvision.cn>
2021-10-13 23:27:02 +0800

    config.json可以使用_exports属性进行文件扩展

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name='zoo-framework',  # 包名
-      version='0.1.20',  # 版本号
+      version='0.2.1',  # 版本号
       description='A simple and quick multi-threaded framework',
       long_description_content_type="text/markdown",
       long_description=long_description,
--- zoo_framework/core/params_factory.py
+++ zoo_framework/core/params_factory.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+from zoo_framework.utils import FileUtils
+
 
 class ParamsFactory:
     config_params = {}
@@ -13,6 +15,35 @@ class ParamsFactory:
 
         with open(config_path, "r") as f:
             ParamsFactory.config_params = json.load(f)
+
+        # 处理 exports
+        self.load_exports()
+
+    def load_exports(self):
+        export_files = self.config_params.get("_exports")
+        if type(export_files) != type([]):
+            return
+
+        for export_file in export_files:
+            self.load_export_file(export_file)
+
+    def load_export_file(self, export_name):
+        file_name = "./" + export_name + ".json"
+
+        if not FileUtils.file_exists(file_name):
+            return
+        content = self.get_export_file(file_name)
+        ParamsFactory.config_params[export_name] = content
+
+    def get_export_file(self, file_name):
+        content = {}
+        try:
+            with open(file_name, "r") as fp:
+                content = json.load(fp)
+        except:
+            pass
+
+        return content
 
     @classmethod
     def get_params(cls, path, default_value=""):
