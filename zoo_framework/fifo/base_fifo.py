de69e66f44276e4f72284703d5c5de300a383d03
mengxiang <xmeng@elsvision.cn>
2021-12-31 00:35:48 +0800

    修改fifo bug

--- zoo_framework/fifo/base_fifo.py
+++ zoo_framework/fifo/base_fifo.py
@@ -3,19 +3,27 @@ class BaseFIFO(object):
     
     def __init__(self):
         pass
-    
-    def push_value(self, value):
-        self.fifo.append(value)
-    
-    def pop_value(self):
-        return self.fifo.pop(0)
-    
-    def push_values(self, values):
-        self.fifo.extend(values)
-    
+
+    @classmethod
+    def push_value(cls, value):
+        cls.fifo.append(value)
+
+    @classmethod
+    def pop_value(cls):
+        if len(cls.fifo) <= 0:
+            return None
+
+        return cls.fifo.pop(0)
+
+    @classmethod
+    def push_values(cls, values):
+        cls.fifo.extend(values)
+
+    @classmethod
     def size(self):
         return len(self.fifo)
-    
-    def push_values_if_null(self, value):
-        if self.fifo.index(value) == -1:
-            self.fifo.append(value)
+
+    @classmethod
+    def push_values_if_null(cls, value):
+        if cls.fifo.index(value) == -1:
+            cls.fifo.append(value)
