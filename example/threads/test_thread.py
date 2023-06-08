44f8b87f4ed1689cc15b6f8b97978dafc388e409
mengxiang <xmeng@elsvision.cn>
2023-06-09 00:06:34 +0800

    perf: 添加必要注释

--- example/threads/test_thread.py
+++ example/threads/test_thread.py
@@ -6,7 +6,7 @@ from zoo_framework import LogUtils, worker, StateMachineManager
 from zoo_framework.workers import BaseWorker
 
 
-@worker(count=6)
+@worker(count=2)
 class TestThread(BaseWorker):
     def __init__(self):
         BaseWorker.__init__(self, {
@@ -15,12 +15,13 @@ class TestThread(BaseWorker):
             "name": "TestThread"
         })
         self.is_loop = True
+        self.i = 0
 
     @staticmethod
     def _on_test_number_change(data):
         value = data.get('value')
         version = data.get('version')
-        LogUtils.info("Test", f"Test number change to {value}-{version}")
+        LogUtils.info("Test", f"Test number change to {value}")
 
     def _on_create(self):
         StateMachineManager().set_state("Test", "Test.number", 0)
@@ -31,7 +32,7 @@ class TestThread(BaseWorker):
 
         state = StateMachineManager().get_state("Test", "Test.number")
         i = state.get_value()
-        LogUtils.info("Test", i)
-        i += 1
-        StateMachineManager().set_state("Test", "Test.number", i)
+        # LogUtils.info("Test", i)
+        self.i += 1
+        StateMachineManager().set_state("Test", "Test.number", self.i)
         sleep(1)
--- zoo_framework/core/waiter/base_waiter.py
+++ zoo_framework/core/waiter/base_waiter.py
@@ -14,8 +14,11 @@ from multiprocessing import Pool
 
 
 class BaseWaiter(object):
+    '''
+    基础的waiter
+    '''
     _lock = None
-    
+
     def __init__(self):
         from zoo_framework.params import WorkerParams
         # 获得模式
@@ -27,52 +30,52 @@ class BaseWaiter(object):
         self.workers = []
         self.worker_props = {}
         self.register_handler()
-    
+
     def get_worker_mode(self, pool_enable):
         if pool_enable:
             return WaiterConstant.WORKER_MODE_THREAD_POOL, pool_enable
         else:
             return WaiterConstant.WORKER_MODE_THREAD, pool_enable
-    
+
     def register_handler(self):
         from zoo_framework.handler.handler_register import HandlerRegister
         HandlerRegister().register("waiter", WaiterResultHandler())
-    
+
     def init_lock(self):
         pass
-    
+
     # 集结worker们
     def call_workers(self, worker_list):
         self.workers = worker_list
-        
+
         # 生成池或者列表
         if self.worker_mode == WaiterConstant.WORKER_MODE_THREAD_POOL:
             self.resource_pool = ThreadPoolExecutor(max_workers=self.pool_size)
-    
+
     def __del__(self):
         if self.resource_pool is not None:
             self.resource_pool.shutdown(wait=True)
-    
+
     # 执行服务
     def execute_service(self):
         workers = []
         for worker in self.workers:
             self.worker_band(worker.name)
-            
+
             if worker is None:
                 continue
-            
+
             if worker.is_loop:
                 workers.append(worker)
-                
+
             # 判定是否超时
             self.worker_band(worker.name)
-            
+
             if self.worker_props.get(worker.name) is None:
                 self._dispatch_worker(worker)
-        
+
         self.workers = workers
-    
+
     def _dispatch_worker(self, worker):
         '''
         派遣 worker
@@ -88,8 +91,12 @@ class BaseWaiter(object):
             t = Thread(target=self.worker_running, args=(worker, self.worker_running_callback))
             t.start()
             self.register_worker(worker, t)
-    
+
     def worker_band(self, worker_name):
+        '''
+        绑定worker
+        :param worker_name: worker的名字
+        '''
         # 根据模式
         worker_prop = self.worker_props.get(worker_name)
         if worker_prop is None:
@@ -99,15 +106,15 @@ class BaseWaiter(object):
         run_time = worker_prop.get("run_time")
         run_timeout = worker_prop.get("run_timeout")
         container = worker_prop.get("container")
-        
+
         now_time = time.time()
 
         if run_timeout is None or run_timeout <= 0:
             return
-        
+
         if (now_time - run_time) < run_timeout:
             return
-        
+
         #
         # if self.worker_mode is WaiterConstant.WORKER_MODE_THREAD_POOL:
         #     if container.cancel() is False:
@@ -116,8 +123,7 @@ class BaseWaiter(object):
         #     container.kill()
         #
         # self.unregister_worker(worker)
-        
-    
+
     def register_worker(self, worker, worker_container):
         '''
         register the worker to self.worker_props
@@ -131,26 +137,26 @@ class BaseWaiter(object):
             "run_timeout": worker.run_timeout,
             "container": worker_container
         }
-    
+
     def unregister_worker(self, worker):
         if self.worker_props.get(worker.name) is not None:
             del self.worker_props[worker.name]
-    
+
     def worker_running_callback(self, worker):
         self.unregister_worker(worker)
-    
+
     # 派遣worker
     def worker_running(self, worker, callback=None):
         if not isinstance(worker, BaseWorker):
             return
-        
+
         result = worker.run()
-        
+
         if callback is not None:
             callback(worker)
-        
+
         return result
-    
+
     # worker汇报结果
     @staticmethod
     def worker_report(worker):
