136f6146bd2042c1a8803b39e7fde0efeced0a78
YearsAlso <1334713380@qq.com>
2023-12-31 18:15:30 +0800

    ci: 修改test 流水线配置

--- ci_tests.py
+++ ci_tests.py
@@ -2,7 +2,7 @@ import pytest
 
 
 def test_event_reactor():
-    from .zoo_framework.event import EventChannel
+    from zoo_framework.event import EventChannel
     event_channel = EventChannel("test_channel")
     event_channel.get_top()
     print("test_event_reactor")
